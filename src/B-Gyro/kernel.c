b4d358990110b5473418bc72228e63ae6cbd75ce
Ayg0 <taha.eddafili@gmail.com>
2024-12-04 17:03:46 +0100

    added serialputChar and helpers for ports IOs

--- src/B-Gyro/kernel.c
+++ src/B-Gyro/kernel.c
@@ -1,9 +1,17 @@
+# include "arch/i386/ports/serialCommunication/serialIO.h"
+
 int	kmain(void){
 	char *videoMemory = (char*)0xb8000;
 	videoMemory[0] = '4';
 	videoMemory[1] = 0x07;
 	videoMemory[2] = '2';
 	videoMemory[3] = 0x07;
+
+	serialPutCharacter('H');
+	serialPutCharacter('e');
+	serialPutCharacter('l');
+	serialPutCharacter('l');
+	serialPutCharacter('o');
 
 	return 0;
 }
