14f83c34e83b7805895e73a0089d18827b634b13
irhesri <rhesri.1337@gmail.com>
2024-12-13 00:08:29 +0100

    switch tty

--- src/include/klibc/memory.h
+++ src/include/klibc/memory.h
@@ -8,3 +8,4 @@ void    *memchr(const void *s, int c, size_t n);
 void    *memcpy(void *dest, const void *src, size_t n);
 void    *memmove(void *dest, const void *src, size_t n);
 int      memcmp(const void *s1, const void *s2, size_t n);
+void	bigBzero(uint16_t *address, uint32_t size);
