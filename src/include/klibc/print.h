6c60b810ac82312f8a4f1324c7055f9cceff7d5d
Ayg0 <taha.eddafili@gmail.com>
2024-12-12 13:39:06 +0100

    integrated putChar in printf-clone

--- src/include/klibc/print.h
+++ src/include/klibc/print.h
@@ -1,4 +1,5 @@
 # pragma once
+# include "terminal/terminal.h"
 # include "arch/i386/ports/serialCommunication/serialIO.h"
 
 typedef uint8_t (*putCharFnc)(char);
@@ -18,3 +19,6 @@ uint32_t     print(putCharFnc fnc, char *fmtString, ...);
 #define SERIAL_SUCC(FMT_STRING, ...)    print(serialPutChar, "\033[92mSUCC \033[39m [%s] " FMT_STRING "\n", __func__, ##__VA_ARGS__)
 #define SERIAL_ERR(FMT_STRING, ...)     print(serialPutChar, "\033[31mERR  \033[39m [%s] " FMT_STRING "\n", __func__, ##__VA_ARGS__)
 #define SERIAL_DEBUG(FMT_STRING, ...)   print(serialPutChar, "\033[93mDEBUG\033[39m [%s] " FMT_STRING "\n", __func__, ##__VA_ARGS__)
+
+
+#define VGA_PRINT(FMT_STRING, ...) print(putChar, FMT_STRING, ##__VA_ARGS__)
