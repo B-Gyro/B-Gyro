97e9e693b57a94810027769739aacae11e537b32
Ayg0 <taha.eddafili@gmail.com>
2024-12-16 17:38:16 +0100

    basic shell added

---
+++ src/B-Gyro/sShell/commands/basicShellCommands.c
@@ -0,0 +1,17 @@
+# include "klibc/print.h"
+# include "klibc/memory.h"
+# include "terminal/tty.h"
+# include "sshell/sshell.h"
+# include "klibc/strings.h"
+
+
+void	sshelClear(char *args) {
+	char *firstArgument;
+
+	firstArgument = strtok(args, " ");
+	if (strncmp(firstArgument, "-h", 2) == 0) {
+		VGA_PRINT("Usage: bro ? you don't know what clear does ? try it !!!\n");
+		return;
+	}
+	clearTTY(SCREEN_SIZE);
+}
